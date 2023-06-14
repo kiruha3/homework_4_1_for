de55731454c4656e8f99ec518842ca9de87b4518
kirill <kiruha3@mail.ru>
2023-06-14 23:04:05 +0700

    9 задача

--- src/Main.java
+++ src/Main.java
@@ -7,58 +7,75 @@ public class Main {
         //task5();
         //task6();
         //task7();
-        task8();
+        //task8();
+        task9();
 
     }
 
     private static void task1() {
-        for (int i = 0; i <=10 ; i++) {
+        for (int i = 0; i <= 10; i++) {
             System.out.println(i);
         }
     }
 
     private static void task2() {
-        for (int i = 10; i >=1 ; i--) {
+        for (int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
     }
 
     private static void task3() {
-        for (int i = 0; i <=17 ; i++) {
-            if (i%2==0) {
+        for (int i = 0; i <= 17; i++) {
+            if (i % 2 == 0) {
                 System.out.println(i);
             }
         }
     }
 
     private static void task4() {
-        for (int i = 10; i >=-10 ; i--) {
+        for (int i = 10; i >= -10; i--) {
             System.out.println(i);
         }
     }
 
     private static void task5() {
-        for (int i = 1904; i <=2096 ; i=i+4) {
-            System.out.println(i+" год является високосным");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
         }
     }
+
     private static void task6() {
-        for (int i = 7; i <=98 ; i=i+7) {
-            System.out.printf(i+" ");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.printf(i + " ");
         }
     }
+
     private static void task7() {
-        for (int i = 1; i <512 ; i=i+i) {
-            System.out.printf(i+" ");
+        for (int i = 1; i < 512; i = i + i) {
+            System.out.printf(i + " ");
         }
     }
+
     private static void task8() {
         int perMonth = 29_000;
         int sumPerYear = 0;
         int year = 12;
-        for (int i = 1; i < year ; i++) {
-            sumPerYear+=perMonth;
-            System.out.println("Месяц "+i +", сумма накоплений равна "+sumPerYear);
+        for (int i = 1; i < year; i++) {
+            sumPerYear += perMonth;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + sumPerYear);
+        }
+    }
+
+    private static void task9() {
+        int perMonth = 29_000;
+        float sumPerYear = 0;
+        int year = 12;
+        float yearPercent = 0.12f;
+        float monthPercent = 1.0f+(yearPercent/year);
+        for (int i = 1; i < year; i++) {
+            sumPerYear += perMonth;
+            sumPerYear = sumPerYear * monthPercent;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + sumPerYear);
         }
     }
 }
