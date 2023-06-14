f6577aec4eec7848dc27dc9df47551f1865a49fa
kirill <kiruha3@mail.ru>
2023-06-14 21:34:56 +0700

    первая и вторая задача

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,35 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    private static void task1() {
+        for (int i = 0; i <=10 ; i++) {
+            System.out.println(i);
         }
+    }
+
+    private static void task2() {
+        for (int i = 10; i >=1 ; i--) {
+            System.out.println(i);
+        }
+    }
+
+    private static void task3() {
+
+    }
+
+    private static void task4() {
+
+
+    }
+
+    private static void task5() {
+
     }
 }
