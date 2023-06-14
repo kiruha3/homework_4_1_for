f1d679a6a56eb8724a1674991ed6e3fafc4a1f44
kirill <kiruha3@mail.ru>
2023-06-14 23:21:15 +0700

    полный набор

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        //task1();
-        //task2();
-        //task3();
-        //task4();
-        //task5();
-        //task6();
-        //task7();
-        //task8();
-        //task9();
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
         task10();
 
     }
 
     private static void task1() {
+        System.out.println("Задача 1");
         for (int i = 0; i <= 10; i++) {
             System.out.println(i);
         }
     }
 
     private static void task2() {
+        System.out.println("Задача 2");
         for (int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
     }
 
     private static void task3() {
+        System.out.println("Задача 3");
         for (int i = 0; i <= 17; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
@@ -34,30 +37,37 @@ public class Main {
     }
 
     private static void task4() {
+        System.out.println("Задача 4");
         for (int i = 10; i >= -10; i--) {
             System.out.println(i);
         }
     }
 
     private static void task5() {
+        System.out.println("Задача 5");
         for (int i = 1904; i <= 2096; i = i + 4) {
             System.out.println(i + " год является високосным");
         }
     }
 
     private static void task6() {
+        System.out.println("Задача 6");
         for (int i = 7; i <= 98; i = i + 7) {
             System.out.printf(i + " ");
         }
+        System.out.println();
     }
 
     private static void task7() {
+        System.out.println("Задача 7");
         for (int i = 1; i < 512; i = i + i) {
             System.out.printf(i + " ");
         }
+        System.out.println();
     }
 
     private static void task8() {
+        System.out.println("Задача 8");
         int perMonth = 29_000;
         int sumPerYear = 0;
         int year = 12;
@@ -68,6 +78,7 @@ public class Main {
     }
 
     private static void task9() {
+        System.out.println("Задача 9");
         int perMonth = 29_000;
         float sumPerYear = 0;
         int year = 12;
@@ -80,6 +91,7 @@ public class Main {
         }
     }
     private static void task10() {
+        System.out.println("Задача 1");
         for (int i=1; i<=10; i++){
             System.out.println("2*"+i+"="+2*i);
         }
